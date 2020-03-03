2fa2077a9320a68f35e9ce75dac72219242d138d
Taylor-Mendez <tlmendez0302@my.msutexas.edu>
2020-03-03 14:20:52 -0600

    update PlayingCard.hpp

--- Assignments/H05/PlayingCard.hpp
+++ Assignments/H05/PlayingCard.hpp
@@ -17,6 +17,22 @@ using namespace std;
 
 class Card {
 private:
+    const string spade = "♠";
+    const string diamond = "♦";
+    const string heart = "♥";
+    const string club = "♣";
+
+    const string suits[4] = {"♠", "♦", "♣", "♥"};
+
+    // Black background        blue ,  red , blue , red
+    const string colors2[4] = {"&60", "&20", "&60", "&20"};
+
+    // Colored background      blue  , red  , blue , red
+    const string colors[4] = {"&16", "&12", "&16", "&12"};
+
+    // Card labels (could be "Iron Man" or "Charmander" or "Elf" ... anything)
+    const string ranks[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    
     int suitNum;     // value 0-3 : num index of suit
     int rank;        // 0-13      : num value of rank
     int number;      // 1-52      : unique value determines card
