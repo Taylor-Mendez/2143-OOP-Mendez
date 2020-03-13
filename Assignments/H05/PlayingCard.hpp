435e4fd91c9e03dcb3c30eb8d62a489fa5916641
Taylor-Mendez <tlmendez0302@my.msutexas.edu>
2020-03-13 16:34:35 -0500

    not done confused

--- Assignments/H05/PlayingCard.hpp
+++ Assignments/H05/PlayingCard.hpp
@@ -32,16 +32,44 @@ private:
 
     // Card labels (could be "Iron Man" or "Charmander" or "Elf" ... anything)
     const string ranks[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    
-    int suitNum;     // value 0-3 : num index of suit
-    int rank;        // 0-13      : num value of rank
-    int number;      // 1-52      : unique value determines card
-    int value;       // 1-14      : numeric value used for comparison
-    string suitChar; // "♠", "♦", "♣", "♥"
-    string rankChar; // Ace, 1 ,2 ,3 ... Q, K
-    string color;    // Spade=blue, Diamond=red, etc.
+
+    int suitNum;      // value 0-3 : num index of suit
+    int rank;         // 0-13      : num value of rank
+    int number;       // 1-52      : unique value determines card
+    int value;        // 1-14      : numeric value used for comparison
+    string suitChar;  // "♠", "♦", "♣", "♥"
+    string rankChar;  // Ace, 1 ,2 ,3 ... Q, K
+    string color;     // Spade=blue, Diamond=red, etc.
+    string backcolor; // background color of card
+    string charcolor; // character color of card
 
 public:
+    struct ForeGround{
+        string red;
+        string blue;
+        string black;
+        string defaultcolor;
+
+        ForeGround(){
+            red = "&12";
+            blue = "&15";
+            black = "&11";
+            defaultcolor = "&10"
+        }
+    };
+    struct BackGround{
+        string red;
+        string blue;
+        string black;
+        string defaultcolor;
+
+        BackGround(){
+            red = "&20";
+            blue = "&50";
+            black = "&10";
+            defaultcolor = "&00";
+        }
+    }
     friend ostream;
 
     Card(int);
@@ -51,7 +79,45 @@ public:
     bool operator==(const Card &);
     bool operator!=(const Card &);
     bool operator()(const Card &);
+    void setForegroundColor(string color);       // set foreground color of card
+    void setBackgroundColor(string color);      // set background color of card
+    void setColors(string fore,string back);      // set fore and back
+    void setCharacterColor(string color);        // set symbol color 
+    void setNumberColor(string color);          // set number color
+    void setColors(string fore, string back, string symbol, string number);
 };
+
+void Card::setForegroundColor(string color){
+    if (color == "red")
+        color = ForeGround.red;
+    else if (color == "blue")
+        color = ForeGround.blue;
+    else if (color == "black")
+        color = ForeGround.black;
+    else 
+        color = ForeGround.defaultcolor;
+
+}
+
+void Card::setBackgroundColor(string color){
+    if (color == "red")
+        backcolor = ForeGround.red;
+    else if (color == "blue")
+        backcolor = ForeGround.blue;
+    else if (color == "black")
+        backcolor = ForeGround.black;
+    else 
+        backcolor = ForeGround.defaultcolor;
+}
+
+void Card::setColors(string fore,string back){
+    setForegroundColor(fore);
+    setBackgroundColor(back);
+}
+
+void setCharacterColor(string color){
+    charcolor = setForegroundColor(color);
+}
 
 /**
      * Public : Card
@@ -83,11 +149,12 @@ Card::Card(int num) {
 
     // Card labels (could be "Iron Man" or "Charmander" or "Elf" ... anything)
     const string ranks[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    
     number = num;
     suitNum = number / 13;
     suitChar = suits[suitNum];
     color = colors[suitNum];
+    backcolor;            // not sure what to do yet 
+    charcolor;            // not sure what to do yet
     rank = number % 13;
     rankChar = ranks[rank];
 }
